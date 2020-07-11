cf04b6427ada7b78126f4b46ec8f5c3936404743
BHSKAR PRASAD <varaprasad239@gmail.com>
2020-07-11 09:21:07 +0530

    Update encript.cpp

--- encript.cpp
+++ encript.cpp
@@ -1,14 +1,11 @@
-//#include<bits/stdc++.h>
-#include <iostream> 
-#include <string> 
+#include<bits/stdc++.h> 
 using namespace std; 
+string plaintext = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
   
 // Function generates the encoded text 
 string encoder(string key) 
 { 
     string encoded = ""; 
-    // This array represents the 
-    // 26 letters of alphabets 
     bool arr[26] = {0}; 
   
     // This loop inserts the keyword 
@@ -48,6 +45,44 @@ string encoder(string key)
     return encoded; 
 } 
   
+// This function will decode the message 
+string decipheredIt(string msg, string encoded) 
+{ 
+    // Hold the position of every character (A-Z) 
+    // from encoded string 
+    map <char,int> enc; 
+    for(int i=0; i<encoded.size(); i++) 
+    { 
+        enc[encoded[i]]=i; 
+    } 
+  
+    string decipher=""; 
+  
+    // This loop deciphered the message. 
+    // Spaces, special characters and numbers remain same. 
+    for (int i=0; i<msg.size(); i++) 
+    { 
+        if (msg[i] >='a' && msg[i] <='z') 
+        { 
+            int pos = enc[msg[i]-32]; 
+            decipher += plaintext[pos]; 
+        } 
+        else if(msg[i] >='A' && msg[i] <='Z') 
+        { 
+            int pos = enc[msg[i]]; 
+            decipher += plaintext[pos]; 
+        } 
+        else
+        { 
+            decipher += msg[i]; 
+        } 
+    } 
+    return decipher; 
+} 
+  
+
+  
+  
 // Function that generates encodes(cipher) the message 
 string cipheredIt(string msg, string encoded) 
 { 
@@ -78,10 +113,10 @@ string cipheredIt(string msg, string encoded)
 // Driver code 
 int main() 
 { 
-    // Hold the Keyword 
+// Hold the Keyword 
     string key; 
     string message; 
-    cout << "Enter your Keyword :"; 
+    cout << "Enter your Keyword to Encript your message:"; 
     cin >> key; 
     
     // Function call to generate encoded text 
@@ -97,6 +132,25 @@ int main()
   
     // Function call to print ciphered text 
     cout << "Ciphered Text : " << cipheredIt(message,encoded) << endl; 
+  
+    cout << "Enter your Keyword to decript your message::"; 
+    cin >> key; 
+  
+    // Function call to generate encoded text 
+    string encoded = encoder(key); 
+  
+    // Message that need to decode 
+    
+    cout << "Enter your message : "; 
+    getchar();
+    getline(cin, message); 
+
+    cout << "Message before Deciphering : " << message << endl; 
+  
+    // Function call to print deciphered text 
+    cout << "Deciphered Text : " << decipheredIt(message,encoded) << endl; 
+  
+    
   
     return 0; 
 } 
